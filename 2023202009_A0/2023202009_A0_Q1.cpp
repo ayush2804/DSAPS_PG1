fc03a0e391051ddc6a331a3949410bc8cdbe8cc1
Ayush Pandey <ayushpandey@Ayushs-MacBook-Air.local>
2023-08-16 11:54:56 +0530

    final commit

--- 2023202009_A0/2023202009_A0_Q1.cpp
+++ 2023202009_A0/2023202009_A0_Q1.cpp
@@ -1,31 +1,58 @@
 #include<iostream>
 #include<string>
 using namespace std;
+void merge(string &str, int left, int mid, int right) {
+    int i, j, k;
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    string L = str.substr(left, n1);
+    string R = str.substr(mid + 1, n2);
+
+    i = 0;
+    j = 0;
+    k = left;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            str[k] = L[i];
+            i++;
+        } else {
+            str[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        str[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        str[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(string &str, int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSort(str, left, mid);
+        mergeSort(str, mid + 1, right);
+        merge(str, left, mid, right);
+    }
+}
 bool ana(string a, string b){
-    long long asci1 = 0 ,asci2 = 0;
-        int tempas,temp2as;
-        for(char i: a){
-        tempas = i;
-        asci1+=tempas;
-        if(b.find(i) == string::npos){
-            return false;
-        }
-    }
-    for(char i: b){
-        temp2as = i;
-        asci2+=temp2as;
-        if(a.find(i) == string::npos){
-            return false;
-        }
-    }
-    if(asci1!=asci2){
-        //cout<<asci1<<" "<<asci2;
-        return false;
-    }
-    return true;
+    mergeSort(a, 0, a.length() - 1);
+    mergeSort(b, 0, b.length() - 1);
+    return a==b;
 }
 int main(){
     string a,b;
+    
     cin>>a>>b;
     bool res = ana(a,b);
     if(res){
